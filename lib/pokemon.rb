ad160c9676aa25ea978c22134e559738fef635e4
Adrienne Yao <adrienne.yao@icloud.com>
2019-06-10 20:03:37 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,8 +21,8 @@ class Pokemon
     @all
   end
 
-  def self.save(name, type, db, hp)
-      db.execute("INSERT INTO pokemon (name, type, hp) VALUES (?, ?, ?)", name, type, hp)
+  def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?, ?)", name, type)
     end
 
 end
