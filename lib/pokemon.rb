6fdd5d0a392a93f754308674c9688dbb47a24c0d
Adrienne Yao <adrienne.yao@icloud.com>
2019-06-10 19:47:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,11 @@ class Pokemon
     @@all << self
   end
 
-  def self.find
+  def self.find(find_id, db)
+    def self.find(id_num, db)
+   pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+   Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+ end
   end
 
   def self.all
